8c53ee88f0372a2f9b34d53b0cd1396ca99499f4
Joe Versoza <jversoza@cs.nyu.edu>
2017-03-23 00:05:36 -0400

    add stub app.js with note regarding __dirname and port

---
+++ ait-news/app.js
@@ -0,0 +1,10 @@
+// app.js
+
+// Use __dirname to construct absolute paths for:
+
+// 1. express-static
+// 2. hbs views
+
+// (the instructions have details on how to do this)
+
+// LISTEN ON PORT 3000
